6520bdf0472cdf66bda328ec444c316385f561af
iyujir <48701673+iyujir@users.noreply.github.com>
2019-04-09 15:32:03 +0800

    Revert "Initial commit"
    This reverts commit 7343405dda8584dd5d94e9e62446d11ae52829b0.

--- apps/article/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, re_path
-from apps.article.views import ArticleView
-
-urlpatterns = [
-    path('<int:article_id>/', ArticleView.as_view(), name='article'),  # article浏览
-]
--- apps/user/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, re_path
-
-urlpatterns = [
-    # path('', IndexView.as_view(), name='index'),  # index主页
-]
